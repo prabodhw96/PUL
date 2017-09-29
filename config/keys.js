f77dbd382b241b02b0e4e2b58c48dee3a67f9366
prabodhw96 <prabodhwankhede1996@gmail.com>
2017-09-29 23:23:56 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfNf9sEo59LmvUuUKGlidG9PolaMbE4xQ",
+  authDomain: "pulapp-98f08.firebaseapp.com",
+  databaseURL: "https://pulapp-98f08.firebaseio.com",
+  storageBucket: "pulapp-98f08.appspot.com",
+  messagingSenderId: "636837980669"
+};
+
+//export const lyftClientId = "vIW8hjXUoqPU";
+
+export const googleApiKey = "AIzaSyDZSrM64Ne5KdjSAi2OOgbeZ7rlSZL4s_Y";
+
+export const sentryURL =
+  "https://a63f65fabe714c39bc843d0442a8b86b@sentry.io/124472";
